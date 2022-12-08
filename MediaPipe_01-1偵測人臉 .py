2357cc55b42fde14ff1349c7a254fa980b09bdff
yayayang <hana12312347@gmail.com>
2022-12-08 18:52:18 +0800

    人臉偵測

---
+++ MediaPipe_01-1偵測人臉 .py
@@ -0,0 +1,31 @@
+import cv2
+import mediapipe as mp     # 載入 mediapipe 函式庫
+
+cap = cv2.VideoCapture(0)
+mp_face_detection = mp.solutions.face_detection   # 建立偵測方法
+mp_drawing = mp.solutions.drawing_utils           # 建立繪圖方法
+
+with mp_face_detection.FaceDetection(             # 開始偵測人臉
+    model_selection=0, min_detection_confidence=0.5) as face_detection:
+
+    if not cap.isOpened():                        # 若偵測不到相機
+        print("Cannot open camera")
+        exit()
+    while True:
+        ret, img = cap.read()                     # 讀取照片
+        if not ret:                               # 若收不到幀
+            print("Cannot receive frame")
+            break
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   # 將 BGR 顏色轉換成 RGB
+        results = face_detection.process(img2)        # 偵測人臉
+
+        if results.detections:
+            for detection in results.detections:
+                mp_drawing.draw_detection(img, detection)  # 標記人臉
+
+        cv2.imshow('oxxostudio', img)
+        if cv2.waitKey(5) == ord('q'):
+            break    # 按下 q 鍵停止
+cap.release()
+cv2.destroyAllWindows()    #關閉所有視窗
+
