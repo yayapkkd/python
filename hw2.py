18778dbf608d878c560a24be1f36a8d91bae2656
yayayang <hana12312347@gmail.com>
2022-12-08 19:00:09 +0800

    作業2

---
+++ hw2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 14:26:33 2022
+
+@author: YAYA
+"""
+#************************************************************************
+# Name: 楊雅涵
+# Class: 資管系四年級
+# SID: s08490069
+ # Program Name: hw2.py
+# Function: (1) List
+#(2) del 
+#(3) pop()
+#(4) remove()
+#(5) count()
+#(6) index()
+# Homework: No.1
+# Limitations: (1). XXXXXXX
+# Date: 2022/11/13
+#************************************************************************
+
+
+
+print("************************************************************************")
+print("*                                                                      *")
+print("*                    HW2  程 式 輸 出 報 表                            *")
+print("*                                                                      *")
+print("************************************************************************")
+print("(1)Answer:") #輸出題號1
+fruits=["apple", "pineapple", "banana", "grape", "orange", "watermelon"] #創造fruits串列
+print("fruits=",fruits) #輸出fruits
+print("(2)Answer:") #輸出題號2
+rep1_fruits=fruits[0:3]+fruits[1:2]+fruits[2:4]*2+fruits[4:5]+fruits[3:4]+fruits[4:] #以fruits串列創造rep1_fruits串列
+print("rep1_fruits=",rep1_fruits) #輸出rep1_fruits
+print("\n程式執行完畢時間2022-11-15 01:26:52")
+print("楊雅涵 S08490069")
+print("************************************************************************")
